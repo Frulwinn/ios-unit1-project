5fa11402d5b6747ac7f42b08a2eddd024663c910
Frulwinn <frulwinn@gmail.com>
2019-01-03 13:33:45 -0800

    added model functions

--- Books/Books/Book.swift
+++ Books/Books/Book.swift
@@ -2,15 +2,27 @@ import Foundation
 
 struct Book: Codable {
     let kind: String
-    let author: String
     
     let title: [VolumeInfo]
     struct VolumeInfo: Codable {
         let title: String
     }
     
-    let image: [ImageLinks]
-    struct ImageLinks: Codable {
-        let previewLink: String
+    let author: [VolumeInfo]
+    struct VolumeInfo: Codable {
+        let volumeInfo: Authors
+        
+        struct Authors: Codable {
+            let authors: String
+        }
+    }
+    
+    let image: [VolumeInfo]
+    struct VolumeInfo: Codable {
+        let volumeInfo: ImageLinks
+        
+        struct ImageLinks: Codable {
+            let imageLinks: String
+        }
     }
 }
--- Books/Books/Model.swift
+++ Books/Books/Model.swift
@@ -1,6 +1,89 @@
 import Foundation
 //key=API_KEY
 //api key AIzaSyA1ORJpsHhRC6CENB3wnq02Mb62w2KrWao
+
 class Model {
     
+    //singleton
+    static let shared = Model()
+    private init () {}
+    
+    //baseURL for api
+    private let baseURL = URL(string: "https://www.googleapis.com/auth/books")!
+    private let key = "AIzaSyA1ORJpsHhRC6CENB3wnq02Mb62w2KrWao"
+    //var
+    var book: Book?
+    var books: [Book] = []
+    
+    //count
+    func numberOfBooks() -> Int {
+        return books.count
+    }
+    
+    //access book at index
+    func book(at indexPath: IndexPath) -> Book {
+        return books[indexPath.row]
+    }
+    
+    //add
+    func addBook(book: Book) {
+        books.append(book)
+    }
+    
+    //delete
+    func deleteBook(at indexPath: IndexPath) {
+        books.remove(at: indexPath.row)
+    }
+    
+    //update
+    func updateBook(at indexPath: IndexPath) {
+        let book = books[indexPath.row]
+    }
+    
+    //accessing api process
+    func search(for searchTerm: String, completion: @escaping (Book?, Error?) -> Void ) {
+        
+        let requestURL = baseURL.appendingPathComponent(searchTerm)
+        
+        //put together a url (for URLRequest) to make a dataTask with
+        var components = URLComponents(url: baseURL, resolvingAgainstBaseURL: true)
+        
+        let clientIDQueryItem = URLQueryItem(name: "API_KEY", value: key)
+        let searchQueryItem = URLQueryItem(name: "q", value: searchTerm)
+        components?.queryItems = [clientIDQueryItem, searchQueryItem]
+        
+        //create get request
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = "GET"
+        
+        //fetching data
+        let dataTask = URLSession.shared.dataTask(with: request) { (data, _, error) in
+            if let error = error {
+                NSLog("There was a problem getting data from JSON: \(error)")
+                completion(nil, error)
+                return
+            }
+            guard let data = data else {
+                NSLog("No data found")
+                completion(nil, error)
+                return
+            }
+            
+            do {
+                let jsonDecoder = JSONDecoder()
+                jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
+                
+                let book = try jsonDecoder.decode(Book.self, from: data)
+                self.book = book
+                print("\(book)")
+                completion(book, nil)
+                
+            } catch {
+                NSLog("Unable to decode data: \(error)")
+                completion(nil, error)
+                return
+            }
+        }
+        dataTask.resume()
+    }
 }
