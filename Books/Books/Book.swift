d0e0cf426190792210ad379c9fb2f16edd4269dd
Frulwinn <frulwinn@gmail.com>
2019-01-03 11:34:45 -0800

    made book struct codable

--- Books/Books/Book.swift
+++ Books/Books/Book.swift
@@ -1,8 +1,16 @@
 import Foundation
 
-struct Book {
-    let title: String
-    let image: String
+struct Book: Codable {
+    let kind: String
     let author: String
     
+    let title: [VolumeInfo]
+    struct VolumeInfo: Codable {
+        let title: String
+    }
+    
+    let image: [ImageLinks]
+    struct ImageLinks: Codable {
+        let previewLink: String
+    }
 }
--- Books/Books/Model.swift
+++ Books/Books/Model.swift
@@ -1,5 +1,6 @@
 import Foundation
-
+//key=API_KEY
+//api key AIzaSyA1ORJpsHhRC6CENB3wnq02Mb62w2KrWao
 class Model {
     
 }
