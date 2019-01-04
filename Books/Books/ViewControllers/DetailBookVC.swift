292fec9125d8bb38db90782550dc8ded10dc04a9
Frulwinn <frulwinn@gmail.com>
2019-01-03 16:15:49 -0800

    added outlets to DetailBookViewController

--- Books/Books/ViewControllers/DetailBookVC.swift
+++ Books/Books/ViewControllers/DetailBookVC.swift
@@ -3,6 +3,18 @@ import UIKit
 class DetailBookVC: UIViewController {
     
     var bookItem: BookItem?
-   
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var authorLabel: UILabel!
+    @IBOutlet weak var imageLinksView: UIImageView!
+    @IBOutlet weak var reviewTextView: UITextView!
+    @IBOutlet weak var notReadButton: UIButton!
+    @IBAction func addToRead(_ sender: Any) {
+        
+    }
+    
+    @IBAction func saveButton(_ sender: Any) {
+        
+    }
+    
 }
 
